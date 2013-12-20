ac03f91b78cf20cfb85a82016fa049865e30a1dd
Stanislav Mekhonoshin <ejabberd@gmail.com>
2013-12-20 22:24:29 +0400

    More cleanup

--- Group.java
+++ Group.java
@@ -1,51 +1,35 @@
-/**
- * Created with IntelliJ IDEA.
- * User: k2_32
- * Date: 15.11.13
- * Time: 10:23
- * To change this template use File | Settings | File Templates.
- * класс группы
- */
 public class Group {
-    private Integer courseNumber;
-    private Integer groupNumber;
-    private Integer subGroupNumber;
-    //private List students;
-    //public String toString(){};
+  private Integer courseNumber;
+  private Integer groupNumber;
+  private Integer subGroupNumber;
 
-    public Group(Integer courseNumber, Integer groupNumber, Integer subGroupNumber){
-           this.courseNumber=courseNumber;
-           this.groupNumber=groupNumber;
-           this.subGroupNumber=subGroupNumber;
-    };
+  public Group(Integer courseNumber, Integer groupNumber, Integer subGroupNumber) {
+    this.courseNumber=courseNumber;
+    this.groupNumber=groupNumber;
+    this.subGroupNumber=subGroupNumber;
+  }
 
-    //Считывание  приватного поля
-    public Integer getCourseNumber(){
-        return this.courseNumber;
-    }
+  public Integer getCourseNumber() {
+    return this.courseNumber;
+  }
 
-    //Передача параметра и присваивание значение приватному полю
-    public void setCourseNumber(Integer courseNumber){
-        this.courseNumber = courseNumber;
-    }
+  public void setCourseNumber(Integer courseNumber) {
+    this.courseNumber = courseNumber;
+  }
 
-    //Считывание  приватного поля
-    public Integer getGroupNumber(){
-        return this.groupNumber;
-    }
+  public Integer getGroupNumber() {
+    return this.groupNumber;
+  }
 
-    //Передача параметра и присваивание значение приватному полю
-    public void setGroupNumber(Integer groupNumber){
-        this.groupNumber = groupNumber;
-    }
+  public void setGroupNumber(Integer groupNumber) {
+    this.groupNumber = groupNumber;
+  }
 
-    //Считывание  приватного поля
-    public Integer getSubGroupNumber(){
-        return this.subGroupNumber;
-    }
+  public Integer getSubGroupNumber() {
+    return this.subGroupNumber;
+  }
 
-    //Передача параметра и присваивание значение приватному полю
-    public void setSubGroupNumber(Integer subGroupNumber){
-        this.courseNumber = subGroupNumber;
-    }
+  public void setSubGroupNumber(Integer subGroupNumber) {
+    this.courseNumber = subGroupNumber;
+  }
 }
--- Main.java
+++ Main.java
@@ -1,37 +1,11 @@
 import java.io.*;
 
 public class Main {
+  public static void main(String[] args) {
+    Application app = new Application();
+    Seeder.createData(app);
 
-    public static void main(String[] args) {
-        //Application app = new Application();
-        //Seeder.createData(app); //заполнение данными
-        //ConsoleController consoleController = new ConsoleController(app);
-       // consoleController.run(); //запуск бесконечного эвента
-       String file ="test.dat";
-        RandomAccessFile rf = null;
-        try {
-            rf = new RandomAccessFile(file, "rw");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-        int i;
-        for(i = 0; i < 7; i++){
-            try {
-                System.out.println("Значение " + i + " " + rf.readDouble());
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-            try {
-                System.out.println(rf.readUTF());
-            } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-            }
-        }
-        try {
-            rf.close();
-        } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
-    }
+    ConsoleController consoleController = new ConsoleController(app);
+    consoleController.run();
+  }
 }
