213f63405937f765cceb295f924a11c4fec3bbf3
Stanislav Mekhonoshin <ejabberd@gmail.com>
2014-02-10 23:26:08 +0400

    Рефакторинг сидера

--- ru/vsu/math/java/Seeder.java
+++ ru/vsu/math/java/Seeder.java
@@ -3,24 +3,27 @@ import ru.vsu.math.java.entity.Group;
 
 public class Seeder {
     public static void createData(Application app){
-      String array[];
+      String[] studentNames = {
+        "Соловьев Дмитрий Николаевич",
+        "Савенков Михаил Владиславович",
+        "Новоселов Антон Григорьевич",
+        "Шипилов Алексей Владимирович",
+        "Шилин Семен Иванович",
+        "Микляев Владислав Иванович",
+        "Измайлова Марина Анатольевна",
+        "Щеблыкина Ирина Владимировна",
+        "Кольцова Карелия Бенедиктовна",
+        "Ляхов Владислав Иванович"
+      };
 
       Group group = app.addGroup(2, 3, 2);
 
-      app.createStudent("Соловьев Дмитрий Николаевич", group);
-      app.createStudent("Савенков Михаил Владиславович", group);
-      app.createStudent("Новоселов Антон Григорьевич", group);
-      app.createStudent("Шипилов Алексей Владимирович", group);
-      app.createStudent("Шилин Семен Иванович", group);
-      app.createStudent("Микляев Владислав Иванович", group);
-      app.createStudent("Измайлова Марина Анатольевна", group);
-      app.createStudent("Щеблыкина Ирина Владимировна", group);
-      app.createStudent("Кольцова Карелия Бенедиктовна", group);
-      app.createStudent("Ляхов Владислав Иванович", group);
+      for (String name : studentNames) {
+        app.createStudent(name, group);
+      }
 
       app.addTutor("Прядко Ирина Николаевна");
       app.addTutor("Бахтина Жанна Игоревна");
       app.addTutor("Зубова Светлана Петровна");
-
     };
 }
